ad0b3d1b4a07180530c94c9b367b0a0be90aa34e
Máté Fejes <fema3832@gmail.com>
2023-10-22 14:19:26 +0200

    better edit

--- betoltes.py
+++ betoltes.py
@@ -1,5 +1,11 @@
 # #!/usr/bin/env python
 import os
+def getfiles(dirpath):
+    a = [s for s in os.listdir(dirpath)
+         if os.path.isfile(os.path.join(dirpath, s))]
+    a.sort(key=lambda s: os.path.getmtime(os.path.join(dirpath, s)))
+    return a
+
 default1 = """<!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,7 +15,7 @@ default1 = """<!DOCTYPE html>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <script src="script.js" defer></script>
-    <title>Fejes Máté | Photographer</title>
+    <title>Fejes Máté - Fotó</title>
 
     <meta name="keywords" content="photography,videography,youtube,instagram,photos,fejesmate,fema3832,fema,photographer,developer,fejes,mate">
     <meta name="description" content="Üdvözöllek!
@@ -19,27 +25,29 @@ Fejes Máté vagyok, kezdő fótos, aki szereti megörökíteni az emlékeit és
     <meta property="og:title" content="Fejes Máté | Photographer">
     <meta property="og:description" content="Üdvözöllek!
 Fejes Máté vagyok, kezdő fótos, aki szereti megörökíteni az emlékeit és a szép dolgokat.">
-    <meta property="og:image" content="https://www.photos.fejesmate.hu/photos/EF-6.jpg">
-    <link rel="icon" href="https://www.photos.fejesmate.hu/logo/favicon.ico">
-    <link rel="apple-touch-icon" href="https://www.photos.fejesmate.hu/logo/favicon.ico">
-    <link rel="canonical" href="https://www.photos.fejesmate.hu/">
+    <meta property="og:image" content="https://photos.fejesmate.hu/photos/EF-6.jpg">
+    <link rel="icon" href="https://photos.fejesmate.hu/logo/favicon.ico">
+    <link rel="apple-touch-icon" href="https://photos.fejesmate.hu/logo/favicon.ico">
+    <link rel="canonical" href="https://photos.fejesmate.hu/">
 </head>
 <body>
     <h1 id="title">FEJES MÁTÉ PHOTOGRAPHY</h1>
 
     <div id="fullscreenImageWraper"></div>
     <img src="" alt="" id="fullscreenImage" class="large" loading="lazy"/>
-    <ul id="photolist">
+    <div class="photowrapper">
+        <ul id="photolist">
 """
-default2 = """        <li></li>
-    </ul>
+default2 = """            <li></li>
+        </ul>
+    </div>
 </body>
 </html>
 """
 
 with open("index.html", "w", encoding="utf-8") as f:
     f.write(default1)
-    for images in sorted(os.listdir("photos")):
+    for images in getfiles("D:\Projects\websites\!fejesmate.hu's\photos.fejesmate.hu\photos"):
         if (images.endswith(".jpg")):
-            f.write(f'        <li><img src="./photos/{images}" loading="lazy"/></li>\n')
+            f.write(f'            <li><img src="./photos/{images}" loading="lazy"/></li>\n')
     f.write(default2)
